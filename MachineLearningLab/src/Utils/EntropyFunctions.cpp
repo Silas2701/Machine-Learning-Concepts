5fb2ed43355735bc9f44051728b3c3d0ad7e9669
Silas Meister <silas.meister@stud.h-da.de>
2023-10-04 16:51:19 +0200

    Revert Changes from lab 2

--- MachineLearningLab/src/Utils/EntropyFunctions.cpp
+++ MachineLearningLab/src/Utils/EntropyFunctions.cpp
@@ -21,19 +21,11 @@ double EntropyFunctions::entropy(const std::vector<double>& y) {
 	std::unordered_map<double, int> label_map;
 	double entropy = 0.0;
 	
-	for (const double& label : y) {
-		if (label_map.find(label) == label_map.end()) {
-			label_map[label] = 1;
-		}
-		else {
-			label_map[label]++;
-		}
-	}
+	// Convert labels to unique integers and count their occurrences
+	//TODO
 	
-	for (const auto& pair : label_map) {
-		double probability = static_cast<double>(pair.second) / total_samples;
-		entropy -= probability * log2(probability);
-	}
+	// Compute the probability and entropy
+	//TODO
 
 	return entropy;
 }
@@ -45,23 +37,13 @@ double EntropyFunctions::entropy(const std::vector<double>& y, const std::vector
 	std::unordered_map<double, int> label_map;
 	int total_samples = idxs.size();
 	double entropy = 0.0;
-
 	// Convert labels to unique integers and count their occurrences
-	for (int i : idxs) {
-		double label = y[i];
-		if (label_map.find(label) == label_map.end()) {
-			label_map[label] = 1;
-		}
-		else {
-			label_map[label]++;
-		}
-	}
+	//TODO
+
 
 	// Compute the probability and entropy
-	for (const auto& pair : label_map) {
-		double probability = static_cast<double>(pair.second) / total_samples;
-		entropy -= probability * log2(probability);
-	}
+	//TODO
+
 
 	return entropy;
 }
